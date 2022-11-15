5d3cb8d39c9a5e7d74262f76c1b67b650718033d
huiju1313 <ft179@naver.com>
2022-11-15 12:22:45 +0900

    챕터4 입력

---
+++ src/main/java/j04_입력/Input1.java
@@ -0,0 +1,29 @@
+package j04_입력;
+
+import java.util.Scanner;
+
+public class Input1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = null;
+        int age = 0;
+        String phone = null;
+        String address = null;
+
+        name = scanner.nextLine();
+        age = scanner.nextInt();
+        scanner.nextLine();
+        phone = scanner.nextLine();
+        address = scanner.nextLine();
+
+
+        System.out.println("이름 : " + name );
+        System.out.println("나이 : " + age );
+        System.out.println("연락처 : " + phone );
+        System.out.println("주소 : " + address );
+
+    }
+
+}
