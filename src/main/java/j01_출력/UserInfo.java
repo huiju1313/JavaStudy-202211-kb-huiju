86062faf4c92db13e2812e58116823705aac5814
huiju1313 <ft179@naver.com>
2022-11-15 12:21:52 +0900

    출력1 출력

---
+++ src/main/java/j01_출력/UserInfo.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "고희주";
+        String address = "부산 동래구 사직동";
+        String phone = "010-5699-1786";
+        int age = 25;
+
+        System.out.println(name + "의 주소는 " +address+ " 입니다.");
+        System.out.println(name + "의 연락처는 " +phone+ " 입니다.");
+        System.out.println(name + "의 나이는 " +age+ "살 입니다.");
+    }
+}
